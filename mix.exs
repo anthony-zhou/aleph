904b4951ccfb94f477b063600327bba4d9fc7255
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2022-07-18 22:00:04 -0700

    Add Hexdocs

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Fin.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :crypto],
       mod: {Fin.Application, []}
     ]
   end
@@ -22,6 +22,7 @@ defmodule Fin.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
